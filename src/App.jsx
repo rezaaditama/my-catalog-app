46fbf3618565d90fabbc2303f8b9b24044b7141b
Reza Aditama <aditamareza03@gmail.com>
2024-10-05 15:48:22 +0700

    create component button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Button from './components/Button';
 
 const App = () => {
   return (
-    <div className='flex justify-center items-center min-h-screen bg-blue-700 text-white'>
-      <h1>Hello World</h1>
+    <div className='flex justify-center items-center min-h-screen bg-blue-700 space-x-2'>
+      <Button />
+      <Button />
+      <Button />
     </div>
   );
 };
---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button className='font-bold text-white bg-black px-3 py-2 rounded'>
+      Submit
+    </button>
+  );
+};
+
+export default Button;
