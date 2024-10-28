8f4b33b164bbd932bee5edd2df4875bbc3a8a7ed
Reza Aditama <aditamareza03@gmail.com>
2024-10-29 01:08:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='bg-blue-500 min-h-screen justify-center flex items-center'>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
